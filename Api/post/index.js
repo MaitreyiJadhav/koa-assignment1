c7c556a11b1c0667ac0db988a69a0079ff923f72
MaitreyiJadhav <maitreyijadhav95@gmail.com>
2019-04-02 12:26:16 -0700

    list

--- Api/post/index.js
+++ Api/post/index.js
@@ -34,25 +34,28 @@ const bodyParser = require('koa-bodyparser')
 const cors = require('@koa/cors');
  const pool = require('../../db/db')
 const { twiml: { MessagingResponse } } = require('twilio')
+//const MessagingResponse = require('twilio').twiml.MessagingResponse
+
 const app = new Koa()
 app.use(bodyParser())
 app.use(cors())
-
 app.use(async ctx => {
   const data = await ctx.request.body
   const item = await createPost(data.todoItem)
   const twiml = new MessagingResponse();
   twiml.message(`${req.body}`)
-  ctx.body = `new todoList created, todoItem ${item.inserttodoItem}`
+  ctx.body = `new todoList created, todoItem ${item.todoItem}`
   ctx.type='text/xml';
+  //return twiml.toString();
+
 })
 
 async function createPost(todoItem) {
   try {
     const itemData = await pool.query(`
-      INSERT INTO list(todoItem) 
-      VALUES ("${todoItem}");
-    `)
+      INSERT INTO todoList (todoItem) 
+    VALUES ("${todoItem}");
+     `)
     return itemData
   } catch (error) {
     console.log(error)
